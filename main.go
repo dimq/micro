f24516057f771488696501cfc80bf9b8ad59abc0
b0dian <contact@dimitriquenault.com>
2020-03-10 16:55:27 +0100

    Problem with struct outside of main

--- main.go
+++ main.go
@@ -107,10 +107,6 @@ func main() {
 
 }
 
-type Consumer struct {
-	ready chan bool
-}
-
 // Setup is run at the beginning of a new session, before ConsumeClaim
 func (consumer *Consumer) Setup(sarama.ConsumerGroupSession) error {
 	// Mark the consumer as ready
---
+++ struct.go
@@ -0,0 +1,11 @@
+package main
+
+type Message struct {
+	ID      string `json:"id"`
+	Code    string `json:"code"`
+	Message string `json:"message"`
+}
+
+type Consumer struct {
+	ready chan bool
+}
